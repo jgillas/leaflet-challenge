595f8e9c00da42d122f24e4a1ac1afab446366e1
jgillas <125215083+jgillas@users.noreply.github.com>
2023-06-20 00:17:49 -0500

    update

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,74 +1,72 @@
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-d3.json(queryUrl).then(function (data) {
-    createFeatures(data.features);
+var myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 5,
 });
 
-function markerSize(magnitude) {
-    return magnitude * 2000;
-};
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
 
-function chooseColor(depth) {
-    if (depth < 10) return "#00FF00";
-    else if (depth < 30) return "greenyellow";
-    else if (depth < 50) return "yellow";
-    else if (depth < 70) return "orange";
-    else if (depth < 90) return "orangered";
-    else return "#FF0000";
-}
+d3.json(queryUrl).then(function (data) {
+    function mapStyle(feature) {
+        return {
+            opacity: 1,
+            fillOpacity: 1,
+            fillColor: mapColor(feature.geometry.coordinates[2]),
+            color: "black",
+            radius: mapRadius(feature.properties.mag),
+            stroke: true,
+            weight: 0.5
+        };
+    }
+    function mapColor(depth) {
+        switch (true) {
+            case depth > 90: 
+                return "red";
+            case depth > 70:
+                return "orangered";
+            case depth > 50:
+                return "orange"; 
+            case depth > 30:
+                return "greenyellow";
+            case depth > 10:
+                return "yellow";
+            default:
+                return "lightgreen";
+        }
+    }
+    function mapRadius(mag) {
+        if (mag === 0) {
+            return 1;
+        }
 
-function createFeatures(earthquakeData) {
-    function onEachFeature(feature, layer) {
-        layer.bindPopup(`<h3>Location: ${feature.properties.place}</h3><hr><p>Date: ${new Date(feature.properties.time)}</p><p>Magnitude: ${feature.properties.mag}</p><p>Depth: ${feature.geometry.coordinates[2]}</p>`);
+        return mag * 4;
     }
 
-    var earthquakes = L.geoJSON(earthquakeData, {
-        onEachFeature: onEachFeature,
+    L.geoJson(data, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng);
+        },
 
-        pointToLayer: function(feature, latlng) {
-            var markers = {
-                radius: markerSize(feature.properties.mag),
-                fillColor: chooseColor(feature.geometry.coordinates[2]),
-                fillOpacity: 0.7, 
-                color: "black",
-                stroke: true,
-                weight: 0.5
-            }
-            return L.circle(latlng, markers);
+        style: mapStyle,
+
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place + "<br>Depth: "+ feature.geometry.coordinates[2]);
         }
-    });
+    }).addTo(myMap);
 
-    createMap(earthquakes);
-}
+var legend = L.control({position: "bottomright"});
+legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend"),
+    depth = [-10, 10, 30, 50, 70,90];
 
-function createMap(earthquakes) {
-    var baseMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        tileSize: 500,
-        maxZoom: 15,
-        style: 'mapbox/light-v11'
-    });
-
-    var myMap = L.map("map", {
-        center: [
-            37.09, -95.71
-        ],
-        zoom: 10,
-        layers: [baseMap, earthquakes]
-    });
-
-    var legend = L.control({position: "bottomright"});
-    legend.onAdd = function() {
-        var div = L.DomUtil.create("div", "info legend"),
-        depth = [-10, 10, 30, 50, 70, 90];
-
-        div.innerHTML += "<h3 style='text-align: center'>Depth</h3>"
-
-        for (var i=0; i< depth.length; i++) {
-            div.innerHTML +=
-            '<i style="background:' + chooseColor(depth[i] +1) + '"></i>' + depth[i] + (depth[i+1] ? '&ndash;' + depth[i+1] + '<br>' : '+');
-        }
-        return div; 
-    };
-    legend.addTo(myMap)
+    for (var i=0; i < depth.length; i++) {
+        div.innerHTML +=
+        '<i style="background: ' + mapColor(depth[i] + 1) + '"></i> ' + depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
+    }
+    return div; 
 };
+legend.addTo(myMap)
+});
